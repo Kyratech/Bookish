264f735b756762342ad929a01a724a492c517390
Matthew Langford <Mathew.Langford@softwire.com>
2017-07-12 14:06:04 +0100

    Now can read database, print list of books

--- Bookish.ConsoleApp/Program.cs
+++ Bookish.ConsoleApp/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using Bookish.DataAccess;
+using Dapper;
 
 namespace Bookish.ConsoleApp
 {
@@ -10,6 +15,20 @@ namespace Bookish.ConsoleApp
     {
         static void Main(string[] args)
         {
+            IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            string SqlString = "SELECT * FROM Books";
+            var ourBooks = (List<Book>)db.Query<Book>(SqlString);
+
+            foreach (var book in ourBooks)
+            {
+                Console.WriteLine(new String('-', 20));
+                Console.WriteLine("New book:");
+                Console.WriteLine("ID: " + book.BookId);
+                Console.WriteLine("ISBN: " + book.Isbn);
+                Console.WriteLine("Title: " + book.Title);
+                Console.WriteLine("Author: " + book.Author);
+                Console.WriteLine(new String('-', 20));
+            }
         }
     }
 }
